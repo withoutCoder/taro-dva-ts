11eb2b9407b0a85605bbdaef8d08fa6b0f27556c
paytonwang <paytonwang@tencent.com>
2020-12-05 16:58:54 +0800

    taro ui

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,5 +5,21 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '激励列表'
+      },
+      {
+        pagePath: 'pages/account/index',
+        text: '我的'
+      }
+    ],
+    color: '#8a8a8a',
+    selectedColor: '#2d8cf0',
+    backgroundColor: '#ffffff',
+    borderStyle: 'white'
   }
 };
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -16,7 +16,7 @@ export default class Index extends React.Component {
   render() {
     return (
       <View className="index">
-        <Text>Hello world!</Text>
+        <Text>Hello world! hahah</Text>
       </View>
     );
   }
